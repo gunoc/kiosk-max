d53f21fd4644dea07f04e8cce8290e679d9d767e
gunoc <86706366+gunoc@users.noreply.github.com>
2023-06-22 12:08:49 +0900

    [FE] feat #1: main 상단탭 구현 (#20)
    * [#15] design: 메인화면 레이아웃 구성
    
    * [#15] design: 상단 탭 메뉴 레이아웃 구성
    
    * [#17] feat: 상단 탭메뉴 구현중
    
    * [#17] fix: 탭메뉴 버그 해결
    
    - 드래그가 안되던 문제 해결
    - css 너비 조정
    
    * [#15] design: reset css적용
    
    * [#15] design: 폰트 세팅
    
    * [#15] design: 상단 탭 메뉴 레이아웃 구성

---
+++ fe/src/components/Tab.tsx
@@ -0,0 +1,9 @@
+import classes from './Tab.module.css';
+
+export function Tab({ key, tab }: { key: number; tab: string }) {
+  return (
+    <button className={classes.btn}>
+      <span>{tab}</span>
+    </button>
+  );
+}
---
+++ fe/src/components/TabContainer.tsx
@@ -0,0 +1,51 @@
+import React, { useRef, useState } from 'react';
+import { Tab } from './Tab';
+import classes from './TabContainer.module.css';
+
+export function TabContainer({ tabs }: { tabs: string[] }) {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [startX, setStartX] = useState(0);
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleMouseDown: React.MouseEventHandler = (event) => {
+    setStartX(event.pageX - (containerRef.current?.offsetLeft ?? 0));
+    setIsDragging(true);
+  };
+
+  const handleMouseMove: React.MouseEventHandler = (event) => {
+    if (!isDragging) return;
+    const x = event.pageX - (containerRef.current?.offsetLeft ?? 0);
+    const walk = x - startX;
+    const scrollLeft = containerRef.current?.scrollLeft;
+
+    if (containerRef.current) {
+      containerRef.current.scrollLeft = scrollLeft! - walk;
+      setStartX(x);
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+    setStartX(0);
+  };
+
+  const handleMouseLeave = () => {
+    setIsDragging(false);
+    setStartX(0);
+  };
+
+  return (
+    <div
+      className={classes.container}
+      ref={containerRef}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseLeave}
+    >
+      {tabs.map((tab, index) => (
+        <Tab key={index} tab={tab} />
+      ))}
+    </div>
+  );
+}
