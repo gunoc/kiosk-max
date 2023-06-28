4284696a6ecae7fd20aa33394148a50642bb5a06
JH <vvaa277@gmail.com>
2023-06-29 00:16:46 +0900

    [#68] feat: 카드 결제 진행 - 결제 실패 상황 구현

--- fe/src/components/Modal/Payment.tsx
+++ fe/src/components/Modal/Payment.tsx
@@ -1,26 +1,45 @@
+import { useState } from 'react';
+import { OrderData } from '../../utils/types';
 import { CardPayment } from './CardPayment';
 import { CashPayment } from './CashPayment';
 import classes from './Payment.module.css';
 
-export function Payment({ addModalOpenHandler }: { addModalOpenHandler: (content: any) => void }) {
+export function Payment({ orderList, modalCloseHandler }: { orderList: OrderData[]; modalCloseHandler: () => void }) {
+  const [selectedPayment, setSelectedPayment] = useState<string | null>(null);
+
+  function selectPayHandler(payment: string) {
+    if (payment === 'card') {
+      setSelectedPayment('card');
+    }
+    if (payment === 'cash') {
+      setSelectedPayment('cash');
+    }
+  }
+
   return (
     <>
-      <button
-        className={classes.payBtn}
-        onClick={() => {
-          addModalOpenHandler(<CardPayment />);
-        }}
-      >
-        카드 결제
-      </button>
-      <button
-        className={classes.payBtn}
-        onClick={() => {
-          addModalOpenHandler(<CashPayment />);
-        }}
-      >
-        현금 결제
-      </button>
+      {selectedPayment === null && (
+        <>
+          <button
+            className={classes.payBtn}
+            onClick={() => {
+              selectPayHandler('card');
+            }}
+          >
+            카드 결제
+          </button>
+          <button
+            className={classes.payBtn}
+            onClick={() => {
+              selectPayHandler('cash');
+            }}
+          >
+            현금 결제
+          </button>
+        </>
+      )}
+      {selectedPayment === 'card' && <CardPayment orderList={orderList} modalCloseHandler={modalCloseHandler} />}
+      {selectedPayment === 'cash' && <CashPayment />}
     </>
   );
 }
