da13c4c981225e58c14916eab58156849b28cbbc
JH <vvaa277@gmail.com>
2023-06-29 20:30:57 +0900

    [#85] feat: 카드결제 실패결과창 구현

--- fe/src/components/Loading/PaymentResult.tsx
+++ fe/src/components/Loading/PaymentResult.tsx
@@ -1,6 +1,12 @@
 import classes from './PaymentResult.module.css';
 
-export function PaymentResult({ cause, modalCloseHandler }: { cause: string; modalCloseHandler: () => void }) {
+export function PaymentResult({
+  cause,
+  setIsModalOpen,
+}: {
+  cause: string | undefined;
+  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
   // 타이머로 메인화면돌아가기 //모달 닫기
   // 버튼으로 돌아가기 // 모달 닫기
   return (
@@ -9,7 +15,12 @@ export function PaymentResult({ cause, modalCloseHandler }: { cause: string; mod
         <div className={classes.wrapper}>
           <p className={classes.causeTitle}>결제 실패</p>
           <p className={classes.causeText}>실패 사유: {cause}</p>
-          <button className={classes.btn} onClick={modalCloseHandler}>
+          <button
+            className={classes.btn}
+            onClick={() => {
+              setIsModalOpen(false);
+            }}
+          >
             돌아가기
           </button>
         </div>
